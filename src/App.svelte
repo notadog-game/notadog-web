b8c62c927ca12d492111d50c623a03628ee63990
DenisMobile <danchic95@gmail.com>
2019-07-04 23:08:10 +0500

    init

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,31 @@
 <script>
-	export let name;
+  import Button from "./components/Button.svelte";
+  import Input from "./components/Input.svelte";
+  import PlayButton from "./components/PlayButton.svelte";
+  export let name;
+
+  function testFunction(data) {
+    if (data) {
+      console.log("data :", data);
+    } else {
+      console.log("no-data");
+    }
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+
 </style>
 
-<h1>Hello {name}!!!</h1>
+<div class="container">
+  <h1>Not a Dog!</h1>
+  <h2>Not a Dog!</h2>
+  <Button on:buttonClick={testFunction} text={'NOT A DOG!!!'} />
+  <Input on:inputChange={testFunction} text={'Заполните инпут'} />
+  <span>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo rerum
+    exercitationem maxime quas inventore alias impedit excepturi iste
+    repudiandae obcaecati.
+  </span>
+  <PlayButton />
+</div>
