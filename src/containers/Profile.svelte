7642029e3da74a0ae4020a68b350643a5668ecac
enfacee <enfac@mail.ru>
2019-09-26 00:22:35 +0500

    edit profile links

--- src/containers/Profile.svelte
+++ src/containers/Profile.svelte
@@ -10,22 +10,26 @@
 </script>
 
 <style>
-
+  nav {
+    display: grid;
+    grid-gap: 10px;
+  }
 </style>
 
-<h1>Profile</h1>
-
-<Router {url}>
-  <nav>
-    <Link to="/">Index</Link>
-    <Link to="username">Username</Link>
-    <Link to="email">Email</Link>
-    <Link to="password">Password</Link>
-  </nav>
-  <div>
-    <Route path="/" component={Index} />
-    <Route path="username" component={Username} />
-    <Route path="email" component={Email} />
-    <Route path="password" component={Password} />
-  </div>
-</Router>
+<div class="container">
+  <h1>Profile</h1>
+  <Router {url}>
+    <nav class="subnav">
+      <Link to="/">Index</Link>
+      <Link to="username">Username</Link>
+      <Link to="email">Email</Link>
+      <Link to="password">Password</Link>
+    </nav>
+    <div>
+      <Route path="/" component={Index} />
+      <Route path="username" component={Username} />
+      <Route path="email" component={Email} />
+      <Route path="password" component={Password} />
+    </div>
+  </Router>
+</div>
