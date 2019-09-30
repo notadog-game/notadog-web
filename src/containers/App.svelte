97f44c29979c02a3cc649863ebd9136d227b9f88
sperezhogin <sperezhogin@exadel.com>
2019-09-30 20:45:09 +0300

    fix errors hadling

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -41,7 +41,7 @@
     if (!text) return;
 
     clearNotifications();
-    addNotification({ text, position: "bottom-center", removeAfter: 2500 });
+    addNotification({ text, position: "bottom-center" });
 
     errors.reset();
   });
@@ -50,7 +50,7 @@
 <style>
   @media (min-width: 768px) {
     .app {
-      padding-left: var(--NAD_SIDEBAR_WIDTH);
+      margin-left: var(--NAD_SIDEBAR_WIDTH);
     }
   }
 </style>
